747c1b31ffd5004affe4881f8adb99d96eb7f978
AhmadAbdullahFaqih <97525007+AhmadAbdullahFaqih@users.noreply.github.com>
2022-11-19 09:22:29 +0700

    add UTS PABW

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi.responses import FileResponse
+from fastapi import FastAPI
+import pandas as pd
+
+app = FastAPI()
+
+@app.get('/7B2_Nawasena/data_csv')
+def data_csv():
+    df = pd.read_csv('Cars.csv')
+    df.to_html('Cars.html')
+    return FileResponse('cars.html')
+
+@app.get('/7B2_Nawasena/data_json')
+def data_json():
+    df = pd.read_csv('Cars.csv')
+    df.to_json('Cars.json')
+    return FileResponse('cars.json')
+
+@app.get('/7B2_Nawasena/data_xml')
+def data_xml():
+    # df = pd.read_csv('Cars.csv')
+    # df.to_xml('Cars.xml')
+    return FileResponse('Cars.xml')
+
